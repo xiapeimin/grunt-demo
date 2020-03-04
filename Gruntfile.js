9a4f8a1b59e23d37923dcde4e0b7602ec1a51dac
xiapeimin <2536733090@qq.com>
2020-03-04 12:50:49 +0800

    sprite

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,16 @@
 /* global module: true */
 module.exports = function (grunt) {
   grunt.initConfig({
-    concat: {                               
+    sprite: {
       files: {
-        src: ['rectangle.js','calc.js'],
-        dest: 'dist/bundle.js'
-      } 
+        src: 'images/*.png',
+        dest: 'dist/spritesheet.png',
+        destCss: 'dist/sprites.css'
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-spritesmith');
 
-  grunt.registerTask('default', ['concat']);    
+  grunt.registerTask('default', ['sprite']);
 };
