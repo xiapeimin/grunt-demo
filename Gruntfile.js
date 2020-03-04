100f6fcc4aa16fc40125b08304e37869c5f3dcf4
xiapeimin <2536733090@qq.com>
2020-03-04 11:39:14 +0800

    imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,16 @@
 /* global module: true */
 module.exports = function (grunt) {
   grunt.initConfig({
-    uglify: { 'reactanle.min.js': 'rectangle.js' }
+    imagemin: {                               
+      files: {
+        expand: true,
+        src: ['images/*.{png,jpg,gif}'],
+        dest: 'dist/'
+      }
+    }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['imagemin']);    
 };
-
